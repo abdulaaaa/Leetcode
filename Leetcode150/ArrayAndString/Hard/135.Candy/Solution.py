b356d558ac810e849ccc01d089720e9068ff8782
Abdul Wajid Arikattayil <57371059+abdulaaaa@users.noreply.github.com>
2025-02-11 15:09:16 -0500

    135. Candy

---
+++ Leetcode150/ArrayAndString/Hard/135.Candy/Solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+
+        # neetcode solution
+        arr = [1] * len(ratings)
+
+        for i in range(1, len(ratings)):
+            if ratings[i] > ratings[i - 1]:
+                arr[i] = arr[i - 1] + 1
+
+        for i in range(len(ratings) - 2, -1, -1):
+            if ratings[i] > ratings[i+1]:
+                # we do max here because arr[i + 1] + 1 could potentially decrease the value from what it initially was.
+                arr[i] = max(arr[i], arr[i + 1] + 1)
+
+        return sum(arr)
+
